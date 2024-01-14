e8439373ba871f3a5f195c45b7ccfffb2e9657c1
Christophar Antony <christopharantony@gmail.com>
2024-01-14 13:44:16 +0530

    Deriving Values Outputting Questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
+import QUESTIONS from "../constants/questions";
 
 const Quiz = () => {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
-  return <p>Currently Active Question</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers(prev => [...prev, selectedAnswer])
+  };
+
+  return (
+    <div id="quiz">
+      <div id="questions">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
